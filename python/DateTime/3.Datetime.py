f812e07b23c847353add0a1565bc9751b4a4dfb4
Neetha2325 <neethamallaya@gmail.com>
2026-03-15 13:03:13 +0530

    Rename DateTime/3.Datetime.py to python/DateTime/3.Datetime.py

---
+++ python/DateTime/3.Datetime.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+dob_time=datetime(1990,8,21,11,33,45,9000)
+print(dob_time)
+print(dob_time.year,dob_time.month,dob_time.day,dob_time.hour,dob_time.minute,dob_time.second,dob_time.microsecond)
